bf0e48391cd81354f32e17f937bae7e32d81085e
Marc Otawa <mop.otawa@gmail.com>
2018-12-11 11:05:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/marcotawa/myapplication/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.marcotawa.myapplication;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.student_drawer);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new announcement()).commit();
+            navigationView.setCheckedItem(R.id.nav_announcement);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_announcement:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new announcement()).commit();
+                break;
+            case R.id.nav_grades:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new grades()).commit();
+                break;
+            case R.id.nav_schedule:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new schedule()).commit();
+                break;
+            case R.id.nav_record:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new record()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new profile()).commit();
+                break;
+            case R.id.nav_registration:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new registration()).commit();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings pa ba?", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "wala pangang LOGIN eh!", Toast.LENGTH_LONG).show();
+                break;
+
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
