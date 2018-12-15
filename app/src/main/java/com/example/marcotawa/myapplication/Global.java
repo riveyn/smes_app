52f0aa73b229a011f427e433cca030e34d22e326
Zildjian Mergilla <gtrmergillazildjianl@gmail.com>
2018-12-16 01:00:36 +0800

    added global

---
+++ app/src/main/java/com/example/marcotawa/myapplication/Global.java
@@ -0,0 +1,7 @@
+package com.example.marcotawa.myapplication;
+
+public class Global {
+    public static String DOMAIN="localhost";
+    public static String URL="http://"+Global.DOMAIN+"/smes-perigrinus/assets/php/android.php?";
+    public static String TOKEN="160413763d3bdd6f8a451d5d0c62b668559f9c828a0a29609f5e3e495ecfdc70c01f8175c4ff601f95725434db06c077cb812bd112f61c63b35ceec355cf27ba";
+}
